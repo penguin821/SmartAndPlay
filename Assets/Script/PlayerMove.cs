e162ab99c947377bb7ada5eb9547e3734afd9008
penguin821 <penguin821@naver.com>
2020-07-07 14:57:03 +0900

    2일차+1
    애니메이션 연동완료

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float mouseSensitivity = 200.0f;
-    public float moveSpeed = 40.0f;
-    public float rotateSpeed = 160.0f;
+    private float mouseSensitivity = 200.0f;
+    private float moveSpeed = 10.0f;
+    private float rotateSpeed = 160.0f;
 
     private Animator animator;
 
-    private void Start()
+    void Start()
     {
         animator = GetComponent<Animator>();
     }
@@ -21,12 +21,17 @@ public class PlayerMove : MonoBehaviour
         float ang = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        float amtMove = moveSpeed * Time.deltaTime;
-        float amtRot = rotateSpeed * Time.deltaTime;
+        if (ang != 0 || ver != 0)
+        {
+            float amtMove = moveSpeed * Time.deltaTime;
+            float amtRot = rotateSpeed * Time.deltaTime;
 
-        transform.Translate(Vector3.forward * ver * amtMove);
-        transform.Rotate(Vector3.up * ang * amtRot);
+            transform.Translate(Vector3.forward * ver * amtMove);
+            transform.Rotate(Vector3.up * ang * amtRot);
 
-        animator.SetBool("PlayerRun",true);
+            animator.SetBool("PlayerRun", true);
+        }
+        else
+            animator.SetBool("PlayerRun", false);
     }
 }
